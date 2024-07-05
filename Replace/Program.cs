5b856c6076d2186c9f80b8d62456588cf148c695
Ирина Гайдель <scorpion_nova@tut.by>
2024-07-05 12:38:55 +0300

    ref

--- Replace/Program.cs
+++ Replace/Program.cs
@@ -5,7 +5,6 @@ var mas = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
 var run35 = new Run35(mas);
 var r = run35.ActionConverter();
-
 Console.WriteLine(string.Join(",", r));
 
 //number 2
@@ -13,27 +12,22 @@ var mas2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 60, 10
 
 run35 = new Run35(mas2);
 r = new Run47(mas2, run35).ActionConverter();
-
 Console.WriteLine(string.Join(",", r));
-
 
 //number 3
 var run35new = new Run35new(mas2);
 r = new Run47new(mas2, run35new).ActionConverter();
-
 Console.WriteLine(string.Join(",", r));
 
+//factory
 Console.WriteLine();
 
 Creator cr = new ConcreteCreator1(mas2);
 Product pr = cr.FactoryMethod();
-
-//factory
 Console.WriteLine(string.Join(",", pr.Znach));
 
 cr = new ConcreteCreator2(mas2);
 pr = cr.FactoryMethod();
-
 Console.WriteLine(string.Join(",", pr.Znach));
 
 Console.ReadLine();
